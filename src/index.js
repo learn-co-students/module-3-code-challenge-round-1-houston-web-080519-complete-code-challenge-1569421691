c8efc33a33b5a5759ac2a73326c150cbf8810cb6
resham <resham.arora@gmail.com>
2019-09-25 11:06:19 -0500

    done

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
   console.log('%c DOM Content Loaded and Parsed!', 'color: magenta')
 
-  let imageId = 1 //Enter the id from the fetched image here
+  let imageId = '3471'
 
   const imageURL = `https://randopic.herokuapp.com/images/${imageId}`
 
@@ -9,4 +9,19 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const commentsURL = `https://randopic.herokuapp.com/comments/`
 
+  fetch(`https://randopic.herokuapp.com/images/${imageId}`)
+  .then(function(response){
+    return response.json()
+  })
+  .then(function(imageId){
+    // console.log('imageId')
+    method: GET `https://randopic.herokuapp.com/images/:id`
+    
+    headers: {
+      'Content-Type': 'application/json'}
+  })
+
+
+
 })
+
