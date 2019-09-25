dfde78baf23be6e04daf7afbc38175bb987e912d
Israel <Israel.pro1994@gmail.com>
2019-09-25 11:09:57 -0500

    -

--- src/index.js
+++ src/index.js
@@ -1,16 +1,38 @@
 document.addEventListener('DOMContentLoaded', () => {
   console.log('%c DOM Content Loaded and Parsed!', 'color: magenta')
 
+<<<<<<< HEAD
   let imageId = 3463 //Enter the id from the fetched image here
+=======
+  let imageId = 3743 //Enter the id from the fetched image here
+>>>>>>> b8e4abd94dd14a476e1c4aa544857feb55ded8ed
 
   const imageURL = `https://randopic.herokuapp.com/images/${imageId}`
 
   const likeURL = `https://randopic.herokuapp.com/likes/`
 
   const commentsURL = `https://randopic.herokuapp.com/comments/`
+  var imageSmile = document.createElement(image_id)
+  fetch(imageURL)
+    .then(function(image_id){
+      return image_id.json()
+    })
+    .then(function(image_id){
+      imageSmile = image_id
+    })
+    
 
+    
+  // var imageSmile = document.createElement('img')
+  //  imageSmile.src = `https://upload.wikimedia.org/wikipedia/commons/6/66/An_up-close_picture_of_a_curious_male_domestic_shorthair_tabby_cat.jpg`
+   document.getElementById("image").append(imageSmile);
+  // document.body.append()
+  var image = document.createElement('img')
+  image.src =  `https://randopic.herokuapp.com/images/3743`
+  document.getElementById('image_card').append(image)
 })
 
+<<<<<<< HEAD
 fetch('https://randopic.herokuapp.com/images/3463')
 .then(function(response){
     return response.json()
@@ -40,3 +62,10 @@ fetch('https://randopic.herokuapp.com/images/3463')
 
     
    // closes the second then
+=======
+document.getElementById("like_button").addEventListener("click", function(){
+  console.log("button works")
+  likes = document.getElementById("likes")
+  likes++
+})
+>>>>>>> b8e4abd94dd14a476e1c4aa544857feb55ded8ed
